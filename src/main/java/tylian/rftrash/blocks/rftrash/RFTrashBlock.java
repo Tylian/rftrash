358fcec0e38630585c4a1bec49435a7aae5b28fa
Tylian <immatyger@gmail.com>
2016-09-06 05:56:24 -0400

    added bounding box to rftrashcan, closes #6 for now

--- src/main/java/tylian/rftrash/blocks/rftrash/RFTrashBlock.java
+++ src/main/java/tylian/rftrash/blocks/rftrash/RFTrashBlock.java
@@ -2,6 +2,8 @@ package tylian.rftrash.blocks.rftrash;
 
 import java.util.List;
 
+import javax.annotation.Nullable;
+
 import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import net.minecraft.block.Block;
@@ -9,6 +11,7 @@ import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -16,8 +19,10 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -28,16 +33,30 @@ import tylian.rftrash.compat.waila.IWailaInfoProvider;
 import tylian.rftrash.blocks.rftrash.RFTrashTileEntity;
 
 public class RFTrashBlock extends Block implements ITileEntityProvider, IWailaInfoProvider {
+	protected static final AxisAlignedBB CAN_AABB = new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 0.8125D, 0.9375D);
+	
 	public RFTrashBlock() {
 		super(Material.ROCK);
-		setUnlocalizedName(RFTrash.MODID + ".rftrash");
+		
 		setRegistryName("rftrash");
+		setUnlocalizedName(RFTrash.MODID + ".rftrash");
+		setCreativeTab(RFTrash.creativeTab);
+		
 		GameRegistry.register(this);
 		GameRegistry.register(new ItemBlock(this), getRegistryName());
 		GameRegistry.registerTileEntity(RFTrashTileEntity.class, RFTrash.MODID + "_rftrash");
-		setCreativeTab(RFTrash.creativeTab);
 	}
+	
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        return CAN_AABB;
+    }
 
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
+    }
+	
 	@Override
 	public TileEntity createNewTileEntity(World worldIn, int meta) {
 		return new RFTrashTileEntity();
